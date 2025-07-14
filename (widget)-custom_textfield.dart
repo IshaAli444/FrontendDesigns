1c18c8939529100481eb93371c7cdc695412509e
Isha Ali <148653041+IshaAli444@users.noreply.github.com>
2025-07-15 02:04:19 +0500

    Update (widget)-custom_textfield.dart

--- (widget)-custom_textfield.dart
+++ (widget)-custom_textfield.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+
+  const CustomTextField({
+    required this.label,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: TextField(
+        decoration: InputDecoration(
+            labelText: label,
+            labelStyle: const TextStyle(color: Colors.grey),
+            border: InputBorder.none,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            hintStyle: const TextStyle(color: Colors.black)),
+      ),
+    );
+  }
+}
 
